c01dd3a05406eea6c82b20bb6d5373d7f079e754
Rohit <rohitnehra38@gmail.com>
2023-11-24 11:30:05 +0530

    Commit

---
+++ example/Start.cpp
@@ -0,0 +1,12 @@
+// ProjectC.cpp : Defines the entry point for the application.
+//
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake  ssdjdjsj." << endl;
+	return 0;
+}
